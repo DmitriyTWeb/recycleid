7425eda90f1c0e639ce403e0cf5190322bdae92e
Dmitriy Teterin <teterinda@gmail.com>
2021-04-16 13:31:56 +0300

    Обновляет настройку для разработки

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,36 @@
 "use strict";
 
 var gulp = require("gulp");
-var plumber = require("gulp-plumber");
+// var plumber = require("gulp-plumber");
 var sourcemap = require("gulp-sourcemaps");
+var server = require("browser-sync").create();
 var sass = require("gulp-sass");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
-var server = require("browser-sync").create();
 var csso = require("gulp-csso");
 var rename = require("gulp-rename");
-var imagemin = require("gulp-imagemin");
-var webp = require("gulp-webp");
-var svgstore = require("gulp-svgstore")
-var posthtml = require("gulp-posthtml");
-var include = require("posthtml-include");
 var del = require("del");
-var htmlmin = require("gulp-htmlmin");
-var uglify = require('gulp-uglify');
+var webpack = require("webpack-stream");
+const path = require("path");
 
-gulp.task("css", function () {
-  return gulp.src("source/sass/style.scss")
-    .pipe(plumber())
-    .pipe(sourcemap.init())
-    .pipe(sass())
-    .pipe(gulp.dest("build/css"))
-    .pipe(postcss([ autoprefixer() ]))
-    .pipe(csso())
-    .pipe(rename("style.min.css"))
-    .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("build/css"))
-    .pipe(server.stream());
+const webpackConfig = require('./webpack.config.js');
+const webpackConfigProd = Object.assign({}, webpackConfig, { mode: 'production' });
+
+gulp.task('webpack', function () {
+  return gulp
+    .src('src/index.js')
+    .pipe(
+      webpack(require('./webpack.config.js'))
+    )
+    .pipe(gulp.dest(path.resolve(__dirname, 'build')));
+});
+gulp.task('webpackProduction', function () {
+  return gulp
+    .src('src/index.js')
+    .pipe(
+      webpack(webpackConfigProd)
+    )
+    .pipe(gulp.dest(path.resolve(__dirname, 'build')));
 });
 
 gulp.task("server", function () {
@@ -41,9 +42,10 @@ gulp.task("server", function () {
     ui: false
   });
 
-  gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
-  gulp.watch("source/img/icon-*.svg", gulp.series("html", "refresh"));
-  gulp.watch("source/*.html", gulp.series("html", "refresh"));
+  gulp.watch("src/sass/**/*.{scss,sass}", gulp.series("css"));
+  gulp.watch("src/*.html", gulp.series("copy", "refresh"));
+  gulp.watch("src/components/**/*.jsx", gulp.series("webpack", "refresh"));
+  gulp.watch("src/*.js", gulp.series("webpack", "refresh"));
 });
 
 gulp.task("refresh", function (done) {
@@ -51,58 +53,33 @@ gulp.task("refresh", function (done) {
   done();
 });
 
-gulp.task("images", function() {
-  return gulp.src("source/img/**/*.{png,jpg,svg}")
-    .pipe(imagemin([
-      imagemin.optipng({optimizationLevel: 3}),
-      imagemin.jpegtran({progressive: true}),
-      imagemin.svgo()
-
-    ]))
-
-    .pipe(gulp.dest("source/img"));
-
-});
-
-gulp.task("webp", function () {
-  return gulp.src("source/img/**/*.{png,jpg}")
-    .pipe(webp({quality: 90}))
-    .pipe(gulp.dest("source/img"));
-});
-
-gulp.task("html", function () {
-  return gulp.src("source/*.html")
-    .pipe(posthtml([
-      include()
-    ]))
-    .pipe(htmlmin({collapseWhitespace: true}))
-    .pipe(gulp.dest("build"));
-});
-
-gulp.task("jscompress", function () {
-  return gulp.src("source/js/*.js")
-    .pipe(uglify())
-    .pipe(rename({
-      suffix: ".min"
-    }))
-    .pipe(gulp.dest("build/js"));
+gulp.task("css", function () {
+  return gulp.src("src/sass/style.scss")
+    // .pipe(plumber())
+    .pipe(sourcemap.init())
+    .pipe(sass())
+    .pipe(gulp.dest("build/css"))
+    .pipe(postcss([autoprefixer()]))
+    .pipe(csso())
+    .pipe(rename("style.min.css"))
+    .pipe(sourcemap.write("."))
+    .pipe(gulp.dest("build/css"))
+    .pipe(server.stream());
 });
 
 gulp.task("copy", function () {
   return gulp.src([
-    "source/fonts/**/*.{woff,woff2}",
-    "source/img/*.{png,jpg,webp,svg}",
-    "source/js/**",
-    "source/*.ico"
-    ], {
-      base: "source"
-    })
-  .pipe(gulp.dest("build"));
+    "src/*.html",
+  ], {
+
+  })
+    .pipe(gulp.dest("build"));
 });
 
 gulp.task("clean", function () {
   return del("build");
 });
 
-gulp.task("build", gulp.series("clean", "copy", "css", "html", "jscompress"));
-gulp.task("start", gulp.series("build", "server"));
+gulp.task("buildProduction", gulp.series("clean", "copy", "css", "webpackProduction"));
+gulp.task("build", gulp.series("clean", "copy", "css", "webpack"));
+gulp.task("start", gulp.series("build", "webpack", "server"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,30 @@
-const MomentLocalesPlugin = require(`moment-locales-webpack-plugin`);
 const path = require(`path`);
 
 module.exports = {
-  mode: `development`,
-  entry: `./src/main.js`,
+  entry: `./src/index.js`,
+  mode: 'development',
   output: {
     filename: `bundle.js`,
-    path: path.join(__dirname, `public`)
   },
-  devtool: `source-map`,
   devServer: {
-    contentBase: path.join(__dirname, `public`),
-    watchContentBase: true
+    contentBase: path.resolve(__dirname, `build`),
+    open: false,
+    port: 1337,
+    historyApiFallback: true
   },
   module: {
     rules: [
       {
-        test: /\.css$/i,
-        use: [`style-loader`, `css-loader`]
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: `babel-loader`,
+        },
       }
-    ]
+    ],
   },
-  plugins: [
-    new MomentLocalesPlugin({
-      localesToKeep: [`es-us`]
-    })
-  ]
+  resolve: {
+    extensions: [`.js`, `.jsx`]
+  },
+  devtool: `source-map`,
 };
