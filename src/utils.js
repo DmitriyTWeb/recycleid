f0779fda9e555898ce6bb6d6f91546b49569af9c
Dmitriy Teterin <teterinda@gmail.com>
2021-05-27 23:24:45 +0300

    Модель MobileNet изменена на модель TeachableMachine

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,14 @@
-export const extend = (a, b) => Object.assign({}, a, b);
+export const extend = (a, b) => ({ ...a, ...b });
+
+const loadModel = async () => {
+  // Load the image model and setup the webcam
+  const URL = './my_model/';
+  const modelURL = `${URL}model.json`;
+  const metadataURL = `${URL}metadata.json`;
+
+  const model = await tmImage.load(modelURL, metadataURL);
+  return model;
+};
 
 export const getPredictions = (imageURL) => {
   const IMAGE_DEFAULT_WIDTH = 640;
@@ -11,8 +21,9 @@ export const getPredictions = (imageURL) => {
   return new Promise((resolve, reject) => {
     img.src = imageURL;
     img.onload = async () => {
-      const model = await mobilenet.load();
-      const predictions = await model.classify(img);
+      // eslint-disable-next-line no-undef
+      const model = await loadModel();
+      const predictions = await model.predict(img);
       if (predictions) resolve(predictions);
       else reject(new Error('something went wrong while predicions'));
     };
