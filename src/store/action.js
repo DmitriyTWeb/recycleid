5e94116cb5c0a14fa7841b748f44d2fbbc3361c4
Dmitriy Teterin <teterinda@gmail.com>
2021-05-29 19:31:18 +0300

    Добавлена обработка сохранения предсказаний в стор

--- src/store/action.js
+++ src/store/action.js
@@ -1,8 +1,13 @@
 export const ActionType = {
   SET_IMAGE_URL: 'SET_IMAGE_URL',
+  SET_PREDICTIONS: 'SET_PREDICTIONS',
 };
 
 export const setImage = (imageURL) => ({
   type: ActionType.SET_IMAGE_URL,
   payload: imageURL,
+});
+export const setPredictions = (predictions) => ({
+  type: ActionType.SET_PREDICTIONS,
+  payload: predictions,
 });
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -2,6 +2,7 @@ import { ActionType } from './action';
 
 const initialState = {
   imageURL: '',
+  predictions: [],
 };
 
 const extend = (a, b) => ({
@@ -12,9 +13,12 @@ const extend = (a, b) => ({
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case ActionType.SET_IMAGE_URL:
-
       return extend(state, {
         imageURL: action.payload,
+      });
+    case ActionType.SET_PREDICTIONS:
+      return extend(state, {
+        predictions: action.payload,
       });
     default:
       return state;
