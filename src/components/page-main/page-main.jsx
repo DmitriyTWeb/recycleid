2fb70eab70649e0d1e75ca2178b489a9a6844ce6
Dmitriy Teterin <teterinda@gmail.com>
2021-05-29 20:58:44 +0300

    PageMain - стилизация

--- src/components/page-main/page-main.jsx
+++ src/components/page-main/page-main.jsx
@@ -9,7 +9,6 @@ import Model from '../../assets/neuronet/Model';
 
 const PageMain = ({ model }) => (
   <main className="page-main container">
-    <p>This is page main block</p>
     <div className="page-main__row">
       <Capturing />
       <OutputDesk model={model} />
