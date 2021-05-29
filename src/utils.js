a7430cf3e1b397546cd2ee1604da43e1bf9b56f3
Dmitriy Teterin <teterinda@gmail.com>
2021-05-29 21:44:08 +0300

    Utils.js - добавлены функции capitalize и kebabToCamelCase

--- src/utils.js
+++ src/utils.js
@@ -1,2 +1,20 @@
+/* eslint-disable comma-dangle */
 // eslint-disable-next-line import/prefer-default-export
 export const extend = (a, b) => ({ ...a, ...b });
+
+const capitalize = (str) => {
+  if (typeof str !== 'string') {
+    return '';
+  }
+  return str.charAt(0).toUpperCase() + str.slice(1);
+};
+
+export const kebabToCamelCase = (str) => {
+  let strParts = str.split('-');
+  strParts = [].concat(
+    strParts[0],
+    strParts.slice(1).map((part) => capitalize(part))
+  );
+
+  return strParts.join('');
+};
