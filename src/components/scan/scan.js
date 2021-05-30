6e4918c08642d3a724a9a591c46262bd7b12de18
Dmitriy Teterin <teterinda@gmail.com>
2021-05-30 21:20:54 +0300

    Scan component - добавлен

---
+++ src/components/scan/scan.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Scan = (props) => {
+  return(
+    <section className="scan">
+      <button type="button" className="scan__shot-btn">Сканировать</button>
+    </section>
+  );
+};
+
+export default Scan;
