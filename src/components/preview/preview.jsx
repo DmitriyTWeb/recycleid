00c3f1dff781056a804d303c5151747d10f3e9fe
Dmitriy Teterin <teterinda@gmail.com>
2021-06-07 22:39:03 +0300

    Preview компонент - добавлены кнопки сброса и повторного запуска камеры

--- src/components/preview/preview.jsx
+++ src/components/preview/preview.jsx
@@ -1,19 +1,46 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { resetImage } from '../../store/action';
 
-const Preview = ({ imgSrc }) => (
+const Preview = ({ imgSrc, cameraBtnClickHandler, resetImgURL }) => (
   <div className="preview">
-    {console.log('imgURL = ', imgSrc)}
     <img className="preview__render" src={imgSrc} alt="Preview предпросмотр" />
     {!imgSrc && <p>Nothing to displa</p>}
+
+    <div className="preview__controls">
+      <button
+        type="button"
+        className="preview__btn preview__btn--camera"
+        onClick={cameraBtnClickHandler}
+      >
+        Take new photo
+      </button>
+      <button
+        type="button"
+        className="preview__btn preview__btn--reset"
+        onClick={resetImgURL}
+      >
+        Reset photo
+      </button>
+    </div>
   </div>
 );
 
 Preview.propTypes = {
   imgSrc: PropTypes.string,
+  resetImgURL: PropTypes.func.isRequired,
+  cameraBtnClickHandler: PropTypes.func.isRequired,
 };
 Preview.defaultProps = {
   imgSrc: '',
 };
 
-export default Preview;
+const mapDispatchToProps = (dispatch) => ({
+  resetImgURL() {
+    dispatch(resetImage());
+  },
+});
+
+export { Preview };
+export default connect(null, mapDispatchToProps)(Preview);
